bf13255dd12a034d73cbba8d5cb5e1ebd265dfac
linchengyi <cl648@st-andrews.ac.uk>
2011-12-08 04:06:37 +0000

    implement step' function in VM.hs support all instructions now

--- VM.hs
+++ VM.hs
@@ -1,7 +1,7 @@
 module VM where
 
 import Redcode
-import Control.Monad.State
+--import Control.Monad.State
 
 -- Get the data stored at location i in the core - if the location
 -- is out of range, it should wrap around.
@@ -26,16 +26,34 @@ absolute l (BInd i) core = l + i + field_val (b_field (readCore (l + i) core))
 addField :: Field -> Field -> Field
 addField x y = x { field_val = field_val x + field_val y } 
 
+subField :: Field -> Field -> Field
+subField x y = x { field_val = field_val x - field_val y } 
+
+mulField :: Field -> Field -> Field
+mulField x y = x { field_val = field_val x * field_val y } 
+
+divField :: Field -> Field -> Field
+divField x y = x { field_val = field_val x `div` field_val y } 
+
+modField :: Field -> Field -> Field
+modField x y = x { field_val = field_val x `mod` field_val y } 
+
 -- Run an instruction at a location in the core.
 -- Returns Nothing if the instruction is invalid
 
 step' :: Int -> Loc -> Core -> Maybe (Core, Int) 
 step' pc (Loc DAT _ _) _ = Nothing
 step' pc (Loc NOP _ _) core = Just (core, pc + 1)
-step' pc (Loc ADD (Val x) b) core 
+step' pc (Loc MOV (Val x) b) core 
     = let bloc = absolute pc b core
           bl = readCore bloc core in
-          Just (writeCore bloc (bl { b_field = addField (b_field bl) (Val x) }) core,
+          Just (writeCore bloc (bl { a_field = (Val x) }) core,
+                pc + 1)
+step' pc (Loc MOV a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core in
+          Just (writeCore bloc (bl { instruction = (instruction l), a_field = (a_field l), b_field = (b_field l) }) core,
                 pc + 1)
 step' pc (Loc ADD a b) core
     = let l = readCore (absolute pc a core) core
@@ -43,7 +61,76 @@ step' pc (Loc ADD a b) core
           bl = readCore bloc core in
           Just (writeCore bloc (bl { b_field = addField (b_field bl) (a_field l) }) core,
                 pc + 1)
-
+step' pc (Loc SUB a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core in
+          Just (writeCore bloc (bl { b_field = subField (b_field bl) (a_field l) }) core,
+                pc + 1)
+step' pc (Loc MUL a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core in
+          Just (writeCore bloc (bl { b_field = mulField (b_field bl) (a_field l) }) core,
+                pc + 1)
+step' pc (Loc DIV a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core in
+          Just (writeCore bloc (bl { b_field = divField (b_field bl) (a_field l) }) core,
+                pc + 1)
+step' pc (Loc MOD a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core in
+          Just (writeCore bloc (bl { b_field = modField (b_field bl) (a_field l) }) core,
+                pc + 1)	
+step' pc (Loc JMP a b) core
+    = let l = readCore (absolute pc a core) core in 
+		  Just (core, pc + (field_val (a_field l)))
+step' pc (Loc JMZ a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core
+		  in if (field_val (b_field bl)) == 0
+				then Just (core, pc + (field_val (a_field l)))
+				else Just (core, pc + 1)	
+step' pc (Loc JMN a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core
+		  in if (field_val (b_field bl)) /= 0
+				then Just (core, pc + (field_val (a_field l)))
+				else Just (core, pc + 1)	
+step' pc (Loc DJN a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core
+		  in if (field_val (b_field bl)) /= 1
+				then Just (core, pc + (field_val (a_field l)))
+				else Just (core, pc + 1)	
+step' pc (Loc SEQ a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core
+		  in if (field_val (a_field l)) == (field_val (b_field bl))
+				then Just (core, pc + 2)	
+				else Just (core, pc + 1)	
+step' pc (Loc SNE a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core
+		  in if (field_val (a_field l)) /= (field_val (b_field bl))
+				then Just (core, pc + 2)	
+				else Just (core, pc + 1)	
+step' pc (Loc SLT a b) core
+    = let l = readCore (absolute pc a core) core
+          bloc = absolute pc b core
+          bl = readCore bloc core
+		  in if (field_val (a_field l)) < (field_val (b_field bl))
+				then Just (core, pc + 2)	
+				else Just (core, pc + 1)	
+				
 -- Call the step' function on each process in the system in turn. If any
 -- execute an invalid instruction (i.e. a DAT) remove them from the process
 -- list. The winner is the last process running.
