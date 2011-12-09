11709ee0e800c377036dfdfa890b0a5cbe9ae11f
Lin Chengyi <cl648@st-andrews.ac.uk>
2011-12-09 00:54:56 +0000

    remove the unneeded libary

--- Graphics/Gloss/Geometry.hs
+++
@@ -1,8 +0,0 @@
-
-module Graphics.Gloss.Geometry
-	( module Graphics.Gloss.Geometry.Angle
-	, module Graphics.Gloss.Geometry.Line )
-where
-import Graphics.Gloss.Geometry.Angle
-import Graphics.Gloss.Geometry.Line
-	
